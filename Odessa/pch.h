b9b1c8bfd2b30e19250df548c5085d4498a92834
Spectrum76 <49938052+Spectrum76@users.noreply.github.com>
2021-01-05 20:27:28 +0530

    Config Change.

--- Odessa/pch.h
+++ Odessa/pch.h
@@ -8,5 +8,31 @@
 #define PCH_H
 
 // add headers that you want to pre-compile here
+#include <stdexcept>
+#include <vector>
+#include <iostream>
+#include <optional>
+#include <cstdlib>
+#include <set>
+#include <memory>
+#include <fstream>
+#include <string>
+#include <array>
+
+#define WIDTH 800
+#define HEIGHT 600
+#define ImageCount 3
+
+#define WIN32_LEAN_AND_MEAN
+#define GLFW_EXPOSE_NATIVE_WIN32
+
+#define GLM_FORCE_SSE42
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <Windows.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 #endif //PCH_H
