4314cd083d943fc6a01e5f8267036283f331b70e
Spectrum76 <49938052+Spectrum76@users.noreply.github.com>
2021-01-11 13:12:25 +0530

    Update Shader.

--- Odessa/Shader/VertexShader.hlsl
+++ Odessa/Shader/VertexShader.hlsl
@@ -1,12 +1,25 @@
 #include <Input.hlsli>
 
+cbuffer Transform : register(b0)
+{
+    row_major matrix Model;
+    row_major matrix iModel;
+}
+
+cbuffer Camera : register(b1)
+{
+    row_major matrix View;
+    row_major matrix Proj;
+    float3 Eye;
+}
+
 PSInput main(float3 Position : POSITION, float2 TexCoord : TEXCOORD, float3 Normal : NORMAL)
 {
     PSInput result;
 
-    result.Position = float4(Position, 1.0f);
+    result.Position = mul(float4(Position, 1.0f), mul(Model, mul(View, Proj)));
     result.TexCoord = TexCoord;
-    result.Normal = Normal;
+    result.Normal = mul(Normal, float3x3(iModel[0].xyz, iModel[1].xyz, iModel[2].xyz));
     
     return result;
 }
